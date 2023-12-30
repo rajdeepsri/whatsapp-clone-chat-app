6b0ef783e44a82a3c989db5fd6f79935628975e8
rajdeepsri <rajdeep1999.srivastav@gmail.com>
2023-12-30 13:05:28 +0530

    updated router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  );
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <PrivateRoute>
+          <Home />
+        </PrivateRoute>
+      ),
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={appRouter} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./style.scss";
 import AuthProvider from "./context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </AuthProvider>
 );
